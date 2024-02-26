587eb3593d2d418855c37cfc9166619f35d53cce
emma-adu-saah <aduemma07@gmail.com>
2024-02-26 12:32:51 +0000

    Hexa

--- 1-print_square.js
+++ 1-print_square.js
@@ -14,8 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-const sizeHex = parseInt(process.argv[2], 16);
-const size = parseInt(sizeHex, 10);
+size = parseInt(process.argv[2], 10);
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
