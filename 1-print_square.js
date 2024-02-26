033cb0e23b5c1d9bdc41c5d2e18be7f5bb8f3974
emma-adu-saah <aduemma07@gmail.com>
2024-02-26 13:02:59 +0000

    hexa trial

--- 1-print_square.js
+++ 1-print_square.js
@@ -7,15 +7,28 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
+let size;
+
+// Check if the argument is in hexadecimal format
+if (process.argv[2].startsWith("0x")) {
+    // Convert hexadecimal to decimal
+    size = parseInt(process.argv[2], 16);
+} else {
+    // Parse the argument as a decimal number
+    size = parseInt(process.argv[2], 10);
+}
+
+if (isNaN(size)) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
+}
 
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
